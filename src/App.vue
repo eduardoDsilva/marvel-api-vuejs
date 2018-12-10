7452eee2ed82ef6aeace957573280d002c82e6d6
Eduardo da Silva <eduardo.2599@hotmail.com>
2018-12-10 17:59:24 -0200

    Desenvolvida as funções e o template para recuperar os quadrinhos da API.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,44 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id="app" class="md-layout">
+    <div class="md-layout-item md-size-33" v-for="quadrinho in quadrinhos" :key="quadrinho.id">
+      <quadrinho
+        :titulo="quadrinho.title"
+        :descricao="quadrinho.description"
+        :imagem="getImagem(quadrinho)"
+      ></quadrinho>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import MarvelApi from '@/services/MarvelAPI';
+  import Quadrinho from '@/components/Quadrinho';
+  export default {
+    name: 'App',
+    components: {
+      Quadrinho
+    },
+    data() {
+      return {
+        quadrinhos: []
+      }
+    },
+    created() {
+      var self = this
+      //chamando a função da MarvelApi passando o argumento de 10 quadrinhos.
+      MarvelApi.getAllComics(10, comics => {
+        self.quadrinhos = comics.data.data.results;
+      })
+    },
+    methods: {
+      getImagem: function(quadrinho) {
+        if (quadrinho.images.length) {
+          return quadrinho.images[0].path + '/portrait_medium.jpg';
+        }
+      }
+    }
+  };
+  }
 </script>
 
 <style>
